fea40c5aa5aaefbfe378e11adda2a0d3ea6ad1e0
Rohit Sharma <rohitsharmaj7@gmail.com>
2018-12-06 20:49:15 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI = 'mongodb://rohit:sharmaj7@ds047592.mlab.com:47592/rsj7' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
